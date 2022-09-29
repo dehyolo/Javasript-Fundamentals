84ad99b784a185cf376c103c0f1eaebf2f21edc1
André Lopes <yonolino@live.com>
2022-09-29 20:44:32 -0300

    First commit

---
+++ Intro/app.js
@@ -0,0 +1,33 @@
+let nname = "john shrimp taco VI";
+let endereco, cep, estado;
+endereco = "rua 0";
+cep = "45698000";
+estado = "SP";
+console.log(nname);
+
+// nomes de variaveis podem começar com letras, _ ou $
+
+//js é esquizofrenico não precisa dizer tipo de variável
+//POR ISSO typescript é melhor, OLHA ISSO
+
+let number = 2.88;
+number = "MUDEI";
+console.log(number);
+
+const firstname = "john";
+const lastname = "jordan";
+const fullName = firstname + " " + lastname;
+
+console.log(fullName);
+
+//quando usa o + em duas string ele concatena, se não, ele transforma em números.
+let number3 = "10";
+let number4 = "23";
+const result2 = number3 - number4;
+const result3 = number3 + number4;
+const result4 = number3 / number4;
+const result5 = number3 * number4;
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
